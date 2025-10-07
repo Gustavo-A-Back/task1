8ada72eb1b7d7a39e1358ebc1b5536fe694fe913
Gustavo Back <136763792+Gustavo-A-Back@users.noreply.github.com>
2025-10-06 23:11:24 -0300

    Update support_files.py

--- support_files.py
+++ support_files.py
@@ -1,41 +1,72 @@
+
 pairs = {
     "BTC": {
         "mercadobitcoin": "BTC-BRL",
         "foxbit": "btcbrl",
-        "binance": "BTCBRL",
-        "bitso": "btc_brl",
+        "novadax": "BTC_BRL",
+        "binance_brl": "BTCBRL",
+        "binance_ars": "BTCARS",
+        "binance_mxn": "BTCMXN",
+        "ripio": "BTC_BRL",
+        "bitso_brl": "btc_brl",
+        "bitso_ars": "btc_ars",
+        "bitso_mxn": "btc_mxn",
         "coinsph": "BTCPHP",
         "bitvavo": "BTC-EUR"
     },
     "ETH": {
         "mercadobitcoin": "ETH-BRL",
+        "novadax": "ETH_BRL",
         "foxbit": "ethbrl",
-        "binance": "ETHBRL",
-        "bitso": "eth_brl",
+        "ripio": "ETH_BRL",
+        "binance_brl": "ETHBRL",
+        "binance_ars": "ETHARS",
+        "binance_mxn": "ETHMXN",
+        "bitso_brl": "eth_brl",
+        "bitso_ars": "eth_ars",
+        "bitso_mxn": "eth_mxn",
         "coinsph": "ETHPHP",
         "bitvavo": "ETH-EUR"
     },
     "USDT": {
         "mercadobitcoin": "USDT-BRL",
         "foxbit": "usdtbrl",
-        "binance": "USDTBRL",
-        "bitso": "usdt_brl",
+        "novadax": "USDT_BRL",
+        "ripio": "USDT_BRL",
+        "binance_brl": "USDTBRL",
+        "binance_ars": "USDTARS",
+        "binance_mxn": "USDTMXN",
+        "bitso_brl": "usdt_brl",
+        "bitso_ars": "usdt_ars",
+        "bitso_mxn": "usdt_mxn",
         "coinsph": "USDTPHP",
         "bitvavo": "USDT-EUR"  #delisted
     },
     "USDC": {
         "mercadobitcoin": "USDC-BRL",
-        "foxbit": "usdcbRl",
-        "binance": "USDCBRL",
-        "bitso": "usdc_brl",
+        "foxbit": "usdcbrl",
+        "novadax": "USDC_BRL",
+        "binance_brl": "USDCBRL",
+        "ripio": "no",
+        "binance_ars": "USDCARS",
+        "binance_mxn": "USDCMXN",
+        "bitso_brl": "usdc_brl",
+        "bitso_ars": "usdc_ars",
+        "bitso_mxn": "usdc_mxn",
         "coinsph": "USDCPHP",
         "bitvavo": "USDC-EUR"
     },
     "SOL": {
         "mercadobitcoin": "SOL-BRL",
         "foxbit": "solbrl",
-        "binance": "SOLBRL",
-        "bitso": "sol_brl",
+        "novadax": "SOL_BRL",
+        "ripio": "SOL_BRL",
+        "binance_brl": "SOLBRL",
+        "binance_ars": "SOLARS",
+        "binance_mxn": "SOLMXN",
+        "bitso_brl": "sol_brl",
+        "bitso_ars": "sol_ars",
+        "bitso_mxn": "sol_mxn",
         "coinsph": "SOLPHP",
         "bitvavo": "SOL-EUR"
     }
@@ -44,3 +75,4 @@ pairs = {
 #================================================
 
 interval_list=["5m","30m"]
+
