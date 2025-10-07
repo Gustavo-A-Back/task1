e1e546ec2d2592736a8c97ec7b9b8212839fba00
Gustavo Back <136763792+Gustavo-A-Back@users.noreply.github.com>
2025-10-06 23:21:32 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -470,6 +470,7 @@ def get_volume_ripio_30m(candleslookback, basepair, interval):
 
             response = requests.get(url, params=params)
             if response.status_code != 200:
+                print({response.status_code}, {response.text})
                 raise Exception(f"Erro na API Ripio: {response.status_code}, {response.text}")
 
             j = response.json()
@@ -511,6 +512,7 @@ def get_volume_ripio_30m(candleslookback, basepair, interval):
         return result
 
     except Exception as e:
+        print({response.status_code}, {response.text})
         return []
 
 def ripio_aggregate_trades_to_candles(basepair, trades, interval="30m"):
@@ -593,5 +595,6 @@ def authenticator (user,password):
         return True
     else:
         return False
+
 
 
